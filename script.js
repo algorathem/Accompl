a6e9740a79a0d7c8b4afd5a1ea4f43def13fbb39
kcl <kcleck06@gmail.com>
2024-05-27 22:36:51 +0800

    color

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
+
+
 (() => {
     const updateButton = document.getElementById("addNewBook");
     const cancelButton = document.getElementById("cancel");
     const dialog = document.getElementById("bookReviewDialog");
     dialog.returnValue = "bookReviewDialog";
 
-    function openCheck(dialog) {
-      if (dialog.open) {
-        console.log("Dialog open");
-      } else {
-        console.log("Dialog closed");
-      }
-    }
-
     // Update button opens a modeless dialog
     updateButton.addEventListener("click", () => {
       dialog.show();
-      openCheck(dialog);
     });
 
     // Form cancel button closes the dialog box
     cancelButton.addEventListener("click", () => {
-      dialog.close("animalNotChosen");
-      openCheck(dialog);
+      dialog.close("bookNotAdded");
     });
   })();
